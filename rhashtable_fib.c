0343bfdac58f52282876d3bf42369bb040633a42
Sachin Paryani <sachin.paryani@gmail.com>
2016-07-21 07:30:09 +0530

    multiple small changes

--- rhashtable_fib.c
+++ rhashtable_fib.c
@@ -60,51 +60,7 @@ static inline u32 rht_obj_hashfn(const void *data, u32 len, u32 seed)
 	return jhash2((const u32 *)(rfxid_fxid(rfxid)->fx_xid), len, seed);
 }
 
-/* Don't make this function inline, it's bigger than it looks like! */
-static void bucket_unlock(struct list_fib_xid_table *lxtbl, u32 bucket)
-	__releases(bucket)
-{
-	xia_lock_table_unlock(lxtbl->fxt_locktbl, hash_bucket(lxtbl, bucket));
-}
-
-/* Routing tables */
-
-/* This function must be called in process context due to virtual memory. */
-static int alloc_buckets(struct fib_xid_buckets *branch, size_t num)
-{
-	struct hlist_head *buckets;
-	size_t size = sizeof(*buckets) * num;
-
-	buckets = kmalloc(size, GFP_KERNEL);
-	if (unlikely(!buckets)) {
-		buckets = vmalloc(size);
-		if (!buckets)
-			return -ENOMEM;
-		pr_warn("XIP %s: the kernel is running out of memory and/or memory is too fragmented. Allocated virtual memory for now; hopefully, it's going to gracefully degrade packet forwarding performance.\n",
-			__func__);
-	}
-	memset(buckets, 0, size);
-	branch->buckets = buckets;
-	branch->divisor = num;
-	return 0;
-}
-
-/* This function must be called in process context due to virtual memory. */
-static inline void free_buckets(struct fib_xid_buckets *branch)
-{
-	if (unlikely(is_vmalloc_addr(branch->buckets)))
-		vfree(branch->buckets);
-	else
-		kfree(branch->buckets);
-	branch->buckets = NULL;
-	branch->divisor = 0;
-}
-
-/* XTBL_INITIAL_DIV must be a power of 2. */
-#define XTBL_INITIAL_DIV 1
-
-static void rehash_work(struct work_struct *work);
-static void list_xtbl_death_work(struct work_struct *work);
+static void rht_xtbl_death_work(struct work_struct *work);
 
 static int rht_xtbl_init(struct xip_ppal_ctx *ctx, struct net *net,
 			  struct xia_lock_table *locktbl,
@@ -228,29 +184,6 @@ static struct fib_xid *rht_fxid_find_rcu(struct fib_xid_table *xtbl,
 	return NULL;
 }
 
-static u32 list_fib_lock_bucket_xid(struct fib_xid_table *xtbl, const u8 *xid)
-	__acquires(xip_bucket_lock)
-{
-	struct list_fib_xid_table *lxtbl = xtbl_lxtbl(xtbl);
-	u32 bucket;
-
-	read_lock(&lxtbl->fxt_writers_lock);
-	bucket = get_bucket(xid, lxtbl->fxt_active_branch->divisor);
-	bucket_lock(lxtbl, bucket);
-
-	/* Make sparse happy with only one __acquires. */
-	__release(bucket);
-
-	return bucket;
-}
-
-static inline u32 list_fib_lock(struct fib_xid_table *xtbl,
-				struct fib_xid *fxid)
-				__acquires(xip_bucket_lock)
-{
-	return list_fib_lock_bucket_xid(xtbl, fxid->fx_xid);
-}
-
 /* For the relativistic hashtable FIB, @parg represents a unsigned int hash. */
 static inline unsigned int parg_hash(void *parg)
 {
@@ -275,13 +208,11 @@ static void rht_fib_unlock(struct fib_xid_table *xtbl, void *parg)
 	spin_unlock_bh(lock);
 }
 
-static struct fib_xid *list_fxid_find_lock(void *parg,
+/* Coming soon */
+static struct fib_xid *rht_fxid_find_lock(void *parg,
 	struct fib_xid_table *xtbl, const u8 *xid) __acquires(xip_bucket_lock)
 {
-	struct hlist_head *head;
-	u32 *pbucket = parg;
-	*pbucket = list_fib_lock_bucket_xid(xtbl, xid);
-	return list_fxid_find_locked(xtbl, *pbucket, xid, &head);
+	return NULL;
 }
 
 /* This function holds and releases an rcu read lock, so the caller must not hold one */  
@@ -339,89 +270,6 @@ static int rht_iterate_xids_rcu(struct fib_xid_table *xtbl,
 				 const void *arg)
 {
 	return 0;
-}
-
-/* Grow table as needed. */
-static void rehash_work(struct work_struct *work)
-{
-	struct list_fib_xid_table *lxtbl = container_of(work,
-		struct list_fib_xid_table, fxt_rehash_work);
-	struct fib_xid_buckets *abranch = lxtbl->fxt_active_branch;
-	int aindex = lxtbl_branch_index(lxtbl, abranch);
-	int nindex = 1 - aindex;
-	/* The next branch. */
-	struct fib_xid_buckets *nbranch = &lxtbl->fxt_branch[nindex];
-	int old_divisor = abranch->divisor;
-	int new_divisor = old_divisor * 2;
-	int mv_count = 0;
-	int rc, i, c, should_rehash;
-
-	/* Allocate memory before aquiring write lock because it sleeps. */
-	BUG_ON(!is_power_of_2(new_divisor));
-	rc = alloc_buckets(nbranch, new_divisor);
-	if (rc) {
-		pr_err(
-		"Rehashing XID table %x was not possible due to error %i.\n",
-			__be32_to_cpu(lxtbl_xtbl(lxtbl)->fxt_ppal_type), rc);
-		dump_stack();
-		return;
-	}
-
-	write_lock(&lxtbl->fxt_writers_lock);
-
-	/* We must test if we @should_rehash again because we may be
-	 * following another rehash_work that just finished.
-	 * Even if we're not following another rehash_work, fxt_count may have
-	 * changed while we waited on write_lock() or to be scheduled, and
-	 * a rehash became unnecessary.
-	 */
-	should_rehash = atomic_read(&lxtbl_xtbl(lxtbl)->fxt_count) /
-				    old_divisor > 2;
-	if (!should_rehash) {
-		/* The calling order here is very important because
-		 * function free_buckets sleeps.
-		 */
-		write_unlock(&lxtbl->fxt_writers_lock);
-		free_buckets(nbranch);
-		return;
-	}
-
-	/* Add entries to @nbranch. */
-	for (i = 0; i < old_divisor; i++) {
-		struct list_fib_xid *lfxid;
-		struct hlist_head *head = &abranch->buckets[i];
-
-		hlist_for_each_entry(lfxid, head, fx_branch_list[aindex]) {
-			struct hlist_head *new_head =
-				xidhead(nbranch, lfxid_fxid(lfxid)->fx_xid);
-			hlist_add_head(&lfxid->fx_branch_list[nindex],
-				       new_head);
-			mv_count++;
-		}
-	}
-	rcu_assign_pointer(lxtbl->fxt_active_branch, nbranch);
-
-	/* It doesn't return an error here because there's nothing
-	 * the caller can do about this error/bug.
-	 */
-	c = atomic_read(&lxtbl_xtbl(lxtbl)->fxt_count);
-	if (c != mv_count) {
-		pr_err("While rehashing XID table of principal %x, %i entries were found, whereas %i are registered! Fixing the counter for now, but it's a serious bug!\n",
-		       __be32_to_cpu(lxtbl_xtbl(lxtbl)->fxt_ppal_type),
-				     mv_count, c);
-		dump_stack();
-		/* "Fixing" bug. */
-		atomic_set(&lxtbl_xtbl(lxtbl)->fxt_count, mv_count);
-	}
-
-	write_unlock(&lxtbl->fxt_writers_lock);
-
-	/* Make sure that there's no reader in @abranch. */
-	synchronize_rcu();
-
-	/* From now on, all readers are using @nbranch. */
-
-	free_buckets(abranch);
 }
 
 /* Coming Soon */
@@ -543,31 +391,31 @@ out:
 }
 
 const struct xia_ppal_rt_iops xia_ppal_rht_rt_iops = {
-	.xtbl_init = list_xtbl_init,
-	.xtbl_death_work = list_xtbl_death_work,
+	.xtbl_init = rht_xtbl_init,
+	.xtbl_death_work = rht_xtbl_death_work,
 
-	.fxid_ppal_alloc = list_fxid_ppal_alloc,
-	.fxid_init = list_fxid_init,
+	.fxid_ppal_alloc = rht_fxid_ppal_alloc,
+	.fxid_init = rht_fxid_init,
 
-	.fxid_find_rcu = list_fxid_find_rcu,
-	.fxid_find_lock = list_fxid_find_lock,
-	.iterate_xids = list_iterate_xids,
-	.iterate_xids_rcu = list_iterate_xids_rcu,
+	.fxid_find_rcu = rht_fxid_find_rcu,
+	.fxid_find_lock = rht_fxid_find_lock,
+	.iterate_xids = rht_iterate_xids,
+	.iterate_xids_rcu = rht_iterate_xids_rcu,
 
-	.fxid_add = list_fxid_add,
-	.fxid_add_locked = list_fxid_add_locked,
+	.fxid_add = rht_fxid_add,
+	.fxid_add_locked = rht_fxid_add_locked,
 
-	.fxid_rm = list_fxid_rm,
-	.fxid_rm_locked = list_fxid_rm_locked,
-	.xid_rm = list_xid_rm,
+	.fxid_rm = rht_fxid_rm,
+	.fxid_rm_locked = rht_fxid_rm_locked,
+	.xid_rm = rht_xid_rm,
 
-	.fxid_replace_locked = list_fxid_replace_locked,
+	.fxid_replace_locked = rht_fxid_replace_locked,
 
-	.fib_unlock = list_fib_unlock,
+	.fib_unlock = rht_fib_unlock,
 
-	.fib_newroute = list_fib_newroute,
-	.fib_delroute = list_fib_delroute,
+	.fib_newroute = rht_fib_newroute,
+	.fib_delroute = rht_fib_delroute,
 
-	.xtbl_dump_rcu = list_xtbl_dump_rcu,
+	.xtbl_dump_rcu = rht_xtbl_dump_rcu,
 };
 EXPORT_SYMBOL_GPL(xia_ppal_rht_rt_iops);
