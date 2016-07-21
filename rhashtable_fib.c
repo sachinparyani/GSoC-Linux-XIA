781c203eda3799578587a7cc739e25c7e49cd32e
Sachin Paryani <sachin.paryani@gmail.com>
2016-07-22 03:27:50 +0400

    Added function rht_fxid_find_rcu

--- rhashtable_fib.c
+++ rhashtable_fib.c
@@ -169,10 +169,40 @@ static struct fib_xid *rht_fxid_find(struct fib_xid_table *xtbl,
 		return NULL;
 }
 
-/* Coming Soon */
 static struct fib_xid *rht_fxid_find_rcu(struct fib_xid_table *xtbl,
 					  const u8 *xid)
 {
+	struct rht_fib_xid_table *rxtbl = xtbl_rxtbl(xtbl);
+	struct rht_fib_xid *rfxid;
+	rfxid = rhashtable_lookup_fast(&rxtbl->rht, xid, rht_params);
+	
+	struct rhashtable_compare_arg arg = {
+		.ht = &rxtbl->rht,
+		.key = xid,
+	};
+	const struct bucket_table *tbl;
+	struct rhash_head *he;
+	unsigned int hash;
+	
+	tbl = rht_dereference_rcu((&rxtbl->rht)->tbl, &rxtbl->rht);
+restart:
+	hash = rht_key_hashfn(&rxtbl->rht, tbl, xid, rht_params);
+	rht_for_each_rcu(he, tbl, hash) {
+		if (params.obj_cmpfn ?
+		    params.obj_cmpfn(&arg, rht_obj(&rxtbl->rht, he)) :
+		    rhashtable_compare(&arg, rht_obj(&rxtbl->rht, he)))
+			continue;
+		
+		return rfxid_fxid(rht_obj(&rxtbl->rht, he));
+	}
+
+	/* Ensure we see any new tables. */
+	smp_rmb();
+
+	tbl = rht_dereference_rcu(tbl->future_tbl, &rxtbl->rht);
+	if (unlikely(tbl))
+		goto restart;
+	
 	return NULL;
 }
 
