de8f04896481881b239f0823458883817f6e685d
sachin.paryani@gmail.com <sachin.paryani@gmail.com>
2016-07-27 06:24:12 +0530

    Updated main.c file of adrht principal

--- xia/ppal_adrht/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_net/xia/ppal_adrht/built-in.o :=  ld -m elf_x86_64   -r -o net/xia/ppal_adrht/built-in.o net/xia/ppal_adrht/xia_ppal_adrht.o 
--- xia/ppal_adrht/main.c
+++ xia/ppal_adrht/main.c
@@ -4,21 +4,21 @@
 #include <net/xia_dag.h>
 #include <net/xia_vxidty.h>
 
-/* Autonomous Domain Principal */
-#define XIDTYPE_AD (__cpu_to_be32(0x10))
+/* Autonomous Domain Relativistic Hash Table Principal */
+#define XIDTYPE_ADRHT (__cpu_to_be32(0x10))
 
-/* AD context */
+/* ADRHT context */
 
-struct xip_ad_ctx {
+struct xip_adrht_ctx {
 	struct xip_ppal_ctx	ctx;
 
 	/* No extra field. */
 };
 
-static inline struct xip_ad_ctx *ctx_ad(struct xip_ppal_ctx *ctx)
+static inline struct xip_adrht_ctx *ctx_adrht(struct xip_ppal_ctx *ctx)
 {
 	return likely(ctx)
-		? container_of(ctx, struct xip_ad_ctx, ctx)
+		? container_of(ctx, struct xip_adrht_ctx, ctx)
 		: NULL;
 }
 
@@ -27,13 +27,13 @@ static int my_vxt __read_mostly = -1;
 /* Use a rht FIB.
  *
  * NOTE
- *	To fully change the rht FIB, you must change @ad_all_rt_eops.
+ *	To fully change the rht FIB, you must change @adrht_all_rt_eops.
  */
-static const struct xia_ppal_rt_iops *ad_rt_iops = &xia_ppal_rht_rt_iops;
+static const struct xia_ppal_rt_iops *adrht_rt_iops = &xia_ppal_rht_rt_iops;
 
-/* Local ADs */
+/* Local ADRHTs */
 
-struct fib_xid_ad_local {
+struct fib_xid_adrht_local {
 	struct xip_dst_anchor   anchor;
 
 	/* WARNING: @common is of variable size, and
@@ -42,10 +42,10 @@ struct fib_xid_ad_local {
 	struct fib_xid		common;
 };
 
-static inline struct fib_xid_ad_local *fxid_lad(struct fib_xid *fxid)
+static inline struct fib_xid_adrht_local *fxid_ladrht(struct fib_xid *fxid)
 {
 	return likely(fxid)
-		? container_of(fxid, struct fib_xid_ad_local, common)
+		? container_of(fxid, struct fib_xid_adrht_local, common)
 		: NULL;
 }
 
@@ -53,26 +53,26 @@ static int local_newroute(struct xip_ppal_ctx *ctx,
 			  struct fib_xid_table *xtbl,
 			  struct xia_fib_config *cfg)
 {
-	struct fib_xid_ad_local *new_lad;
+	struct fib_xid_adrht_local *new_ladrht;
 	int rc;
 
-	new_lad = ad_rt_iops->fxid_ppal_alloc(sizeof(*new_lad), GFP_KERNEL);
-	if (!new_lad)
+	new_ladrht = adrht_rt_iops->fxid_ppal_alloc(sizeof(*new_ladrht), GFP_KERNEL);
+	if (!new_ladrht)
 		return -ENOMEM;
-	fxid_init(xtbl, &new_lad->common, cfg->xfc_dst->xid_id,
+	fxid_init(xtbl, &new_ladrht->common, cfg->xfc_dst->xid_id,
 		  XRTABLE_LOCAL_INDEX, 0);
-	xdst_init_anchor(&new_lad->anchor);
+	xdst_init_anchor(&new_ladrht->anchor);
 
-	rc = ad_rt_iops->fib_newroute(&new_lad->common, xtbl, cfg, NULL);
+	rc = adrht_rt_iops->fib_newroute(&new_ladrht->common, xtbl, cfg, NULL);
 	if (rc)
-		fxid_free_norcu(xtbl, &new_lad->common);
+		fxid_free_norcu(xtbl, &new_ladrht->common);
 	return rc;
 }
 
 /* Based on net/ipv4/fib_semantics.c:fib_dump_info and its call in
  * net/ipv4/fib_trie.c:fn_trie_dump_fa.
  */
-static int local_dump_ad(struct fib_xid *fxid, struct fib_xid_table *xtbl,
+static int local_dump_adrht(struct fib_xid *fxid, struct fib_xid_table *xtbl,
 			 struct xip_ppal_ctx *ctx, struct sk_buff *skb,
 			 struct netlink_callback *cb)
 {
@@ -115,20 +115,20 @@ nla_put_failure:
 }
 
 /* Don't call this function! Use free_fxid instead. */
-static void local_free_ad(struct fib_xid_table *xtbl, struct fib_xid *fxid)
+static void local_free_adrht(struct fib_xid_table *xtbl, struct fib_xid *fxid)
 {
-	struct fib_xid_ad_local *lad = fxid_lad(fxid);
+	struct fib_xid_adrht_local *ladrht = fxid_ladrht(fxid);
 
-	xdst_free_anchor(&lad->anchor);
-	kfree(lad);
+	xdst_free_anchor(&ladrht->anchor);
+	kfree(ladrht);
 }
 
-static const xia_ppal_all_rt_eops_t ad_all_rt_eops = {
+static const xia_ppal_all_rt_eops_t adrht_all_rt_eops = {
 	[XRTABLE_LOCAL_INDEX] = {
 		.newroute = local_newroute,
 		.delroute = rht_fib_delroute,
-		.dump_fxid = local_dump_ad,
-		.free_fxid = local_free_ad,
+		.dump_fxid = local_dump_adrht,
+		.free_fxid = local_free_adrht,
 	},
 
 	XIP_RHT_FIB_REDIRECT_MAIN,
@@ -136,65 +136,65 @@ static const xia_ppal_all_rt_eops_t ad_all_rt_eops = {
 
 /* Network namespace */
 
-static struct xip_ad_ctx *create_ad_ctx(void)
+static struct xip_adrht_ctx *create_adrht_ctx(void)
 {
-	struct xip_ad_ctx *ad_ctx = kmalloc(sizeof(*ad_ctx), GFP_KERNEL);
+	struct xip_adrht_ctx *adrht_ctx = kmalloc(sizeof(*adrht_ctx), GFP_KERNEL);
 
-	if (!ad_ctx)
+	if (!adrht_ctx)
 		return NULL;
-	xip_init_ppal_ctx(&ad_ctx->ctx, XIDTYPE_AD);
-	return ad_ctx;
+	xip_init_ppal_ctx(&adrht_ctx->ctx, XIDTYPE_ADRHT);
+	return adrht_ctx;
 }
 
 /* IMPORTANT! Caller must RCU synch before calling this function. */
-static void free_ad_ctx(struct xip_ad_ctx *ad_ctx)
+static void free_adrht_ctx(struct xip_adrht_ctx *adrht_ctx)
 {
-	xip_release_ppal_ctx(&ad_ctx->ctx);
-	kfree(ad_ctx);
+	xip_release_ppal_ctx(&adrht_ctx->ctx);
+	kfree(adrht_ctx);
 }
 
-static int __net_init ad_net_init(struct net *net)
+static int __net_init adrht_net_init(struct net *net)
 {
-	struct xip_ad_ctx *ad_ctx;
+	struct xip_adrht_ctx *adrht_ctx;
 	int rc;
 
-	ad_ctx = create_ad_ctx();
-	if (!ad_ctx) {
+	adrht_ctx = create_adrht_ctx();
+	if (!adrht_ctx) {
 		rc = -ENOMEM;
 		goto out;
 	}
 
-	rc = ad_rt_iops->xtbl_init(&ad_ctx->ctx, net, &xia_main_lock_table,
-				   ad_all_rt_eops, ad_rt_iops);
+	rc = adrht_rt_iops->xtbl_init(&adrht_ctx->ctx, net, &xia_main_lock_table,
+				   adrht_all_rt_eops, adrht_rt_iops);
 	if (rc)
-		goto ad_ctx;
+		goto adrht_ctx;
 
-	rc = xip_add_ppal_ctx(net, &ad_ctx->ctx);
+	rc = xip_add_ppal_ctx(net, &adrht_ctx->ctx);
 	if (rc)
-		goto ad_ctx;
+		goto adrht_ctx;
 	goto out;
 
-ad_ctx:
-	free_ad_ctx(ad_ctx);
+adrht_ctx:
+	free_adrht_ctx(adrht_ctx);
 out:
 	return rc;
 }
 
-static void __net_exit ad_net_exit(struct net *net)
+static void __net_exit adrht_net_exit(struct net *net)
 {
-	struct xip_ad_ctx *ad_ctx =
-		ctx_ad(xip_del_ppal_ctx(net, XIDTYPE_AD));
-	free_ad_ctx(ad_ctx);
+	struct xip_adrht_ctx *adrht_ctx =
+		ctx_adrht(xip_del_ppal_ctx(net, XIDTYPE_ADRHT));
+	free_adrht_ctx(adrht_ctx);
 }
 
-static struct pernet_operations ad_net_ops __read_mostly = {
-	.init = ad_net_init,
-	.exit = ad_net_exit,
+static struct pernet_operations adrht_net_ops __read_mostly = {
+	.init = adrht_net_init,
+	.exit = adrht_net_exit,
 };
 
-/* AD Routing */
+/* ADRHT Routing */
 
-static int ad_deliver(struct xip_route_proc *rproc, struct net *net,
+static int adrht_deliver(struct xip_route_proc *rproc, struct net *net,
 		      const u8 *xid, struct xia_xid *next_xid,
 		      int anchor_index, struct xip_dst *xdst)
 {
@@ -204,7 +204,7 @@ static int ad_deliver(struct xip_route_proc *rproc, struct net *net,
 	rcu_read_lock();
 	ctx = xip_find_ppal_ctx_vxt_rcu(net, my_vxt);
 
-	fxid = ad_rt_iops->fxid_find_rcu(ctx->xpc_xtbl, xid);
+	fxid = adrht_rt_iops->fxid_find_rcu(ctx->xpc_xtbl, xid);
 	if (!fxid) {
 		xdst_attach_to_anchor(xdst, anchor_index, &ctx->negdep);
 		rcu_read_unlock();
@@ -213,11 +213,11 @@ static int ad_deliver(struct xip_route_proc *rproc, struct net *net,
 
 	switch (fxid->fx_table_id) {
 	case XRTABLE_LOCAL_INDEX: {
-		struct fib_xid_ad_local *lad = fxid_lad(fxid);
+		struct fib_xid_adrht_local *ladrht = fxid_ladrht(fxid);
 
 		xdst->passthrough_action = XDA_DIG;
-		xdst->sink_action = XDA_ERROR; /* An AD cannot be a sink. */
-		xdst_attach_to_anchor(xdst, anchor_index, &lad->anchor);
+		xdst->sink_action = XDA_ERROR; /* An ADRHT cannot be a sink. */
+		xdst_attach_to_anchor(xdst, anchor_index, &ladrht->anchor);
 		rcu_read_unlock();
 		return XRP_ACT_FORWARD;
 	}
@@ -231,65 +231,64 @@ static int ad_deliver(struct xip_route_proc *rproc, struct net *net,
 	BUG();
 }
 
-static struct xip_route_proc ad_rt_proc __read_mostly = {
-	.xrp_ppal_type = XIDTYPE_AD,
-	.deliver = ad_deliver,
+static struct xip_route_proc adrht_rt_proc __read_mostly = {
+	.xrp_ppal_type = XIDTYPE_ADRHT,
+	.deliver = adrht_deliver,
 };
 
-/* xia_ad_init - this function is called when the module is loaded.
+/* xia_adrht_init - this function is called when the module is loaded.
  * Returns zero if successfully loaded, nonzero otherwise.
  */
-static int __init xia_ad_init(void)
+static int __init xia_adrht_init(void)
 {
 	int rc;
 
-	rc = vxt_register_xidty(XIDTYPE_AD);
+	rc = vxt_register_xidty(XIDTYPE_ADRHT);
 	if (rc < 0) {
-		pr_err("Can't obtain a virtual XID type for AD\n");
+		pr_err("Can't obtain a virtual XID type for ADRHT\n");
 		goto out;
 	}
 	my_vxt = rc;
 
-	rc = xia_register_pernet_subsys(&ad_net_ops);
+	rc = xia_register_pernet_subsys(&adrht_net_ops);
 	if (rc)
 		goto vxt;
 
-	rc = xip_add_router(&ad_rt_proc);
+	rc = xip_add_router(&adrht_rt_proc);
 	if (rc)
 		goto net;
 
-	rc = ppal_add_map("ad", XIDTYPE_AD);
+	rc = ppal_add_map("adrht", XIDTYPE_ADRHT);
 	if (rc)
 		goto route;
 
-	pr_alert("XIA Principal AD loaded\n");
+	pr_alert("XIA Principal ADRHT loaded\n");
 	goto out;
 
 route:
-	xip_del_router(&ad_rt_proc);
 net:
-	xia_unregister_pernet_subsys(&ad_net_ops);
+	xia_unregister_pernet_subsys(&adrht_net_ops);
 vxt:
-	BUG_ON(vxt_unregister_xidty(XIDTYPE_AD));
+	BUG_ON(vxt_unregister_xidty(XIDTYPE_ADRHT));
 out:
 	return rc;
 }
 
-/* xia_ad_exit - this function is called when the modlule is removed. */
-static void __exit xia_ad_exit(void)
+/* xia_adrht_exit - this function is called when the modlule is removed. */
+static void __exit xia_adrht_exit(void)
 {
-	ppal_del_map(XIDTYPE_AD);
-	xip_del_router(&ad_rt_proc);
-	xia_unregister_pernet_subsys(&ad_net_ops);
-	BUG_ON(vxt_unregister_xidty(XIDTYPE_AD));
+	ppal_del_map(XIDTYPE_ADRHT);
+	xip_del_router(&adrht_rt_proc);
+	xia_unregister_pernet_subsys(&adrht_net_ops);
+	BUG_ON(vxt_unregister_xidty(XIDTYPE_ADRHT));
 
 	rcu_barrier();
 
 	pr_alert("XIA Principal ADRHT UNloaded\n");
 }
 
-module_init(xia_ad_init);
-module_exit(xia_ad_exit);
+module_init(xia_adrht_init);
+module_exit(xia_adrht_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michel Machado <michel@digirati.com.br>");
